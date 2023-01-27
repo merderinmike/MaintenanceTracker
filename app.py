f53aa44d413b1d200f579066cd9c41bbce0eac49
Alec <alecshehorn97@gmail.com>
2023-01-27 15:17:24 -0600

    z

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
+import pandas
 import streamlit as st
 from db_create import *
-
+from bson.objectid import ObjectId
 #---Page setup---
 title = st.title('Vehicle Maintenance Tracker')
 header = st.subheader('Here you can view, edit, and create Maintenance information related to your specific vehicle')
 tab1, tab2, tab3 = st.tabs(["Add record", "View record", "Add Vehicle"])
+
 #---Add Record---
 with tab1:
-    vehicle = st.selectbox(
-        "Select a vehicle to add a record: ",
-        ()
-    )
-    date = st.date_input("What was the date of Maintenance? ")
-    comment = st.text_area("Comments", placeholder="Please enter a comment")
+    with st.form('addrecord', clear_on_submit=True):
+        vehicle = st.selectbox(
+            "Select a vehicle to add a record: ", (selection)
+        )
+        miles = st.number_input("Enter miles of vehicle", key="miles", value=0, )
+        date = str(st.date_input("What was the date of Maintenance? "))
+        tasks = st.text_area("Tasks", placeholder="What tasks were performed? ")
+        if st.form_submit_button("submit"):
+            records.insert_one({"model": vehicle, "miles":miles, "date": date , "tasks":tasks})
+            st.success("Record created")
 #---View Record---
 with tab2:
-    record = st.selectbox(
-        '',
-        ('Select','option1', 'option2', 'option3')
-    )
-    "---"
-    if 'option1' in record:
-        st.write("You chose option 1!")
+    with st.form("viewRecord"):
+        records = st.selectbox(
+            '',
+            (recordModel)
+        )
+        "---"
+        if st.form_submit_button("submit"):
+            st.table(data=df)
 #---Add Vehicle---
 with tab3:
     with st.form("addVehicle", clear_on_submit=True):
@@ -30,9 +37,11 @@ with tab3:
         year = st.number_input("Please enter the vehicle year: ",key="year" ,value=0)
         submit = st.form_submit_button("submit")
         if submit:
-            if model not in collection:
-                if year not in collection:
-                    collection = db[model]
-                    collection.insert_one({""})
-            st.success("Vehicle submitted")
-        st.write(make, model, year)
+            if vehicles.count_documents({ 'model': model }) != 0:
+                st.exception("Vehicle already exists!")
+            else:
+                vehicles.insert_one({"make": make, "model": model, "year": year})
+                st.success("Vehicle submitted")
+
+
+#if db.collection.count_documents({ 'UserIDS': newID }, limit = 1):
--- db_create.py
+++ db_create.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv, find_dotenv
+from pymongo.collection import Collection
 import os
+import pandas as pd
 
 load_dotenv(find_dotenv())
 
@@ -9,10 +11,21 @@ password = os.environ.get("MONGOPW")
 cluster = MongoClient(
     f"""mongodb+srv://ashehorn:{password}@cluster0.4miwcyq.mongodb.net/?retryWrites=true&w=majority""")
 db = cluster["Maintenance"]
-collection = db["Vehicles"]
+vehicles = db["Vehicles"]
+records = db['Records']
+findVehicle = list(vehicles.find())
+findRecord = list(records.find())
+selection = []
+recordModel = []
+record = []
+#db.Records.insert_one({"model":"tacoma", "miles": 230000,"date":"2023/24/1" ,"tasks":"oil change"})
+for item in findVehicle:
+    selection.append(f"{item['model']}")
 
-# post = {"_id": 0, "Vehicle": "Tacoma", "year": 2006}
-# collection.insert_one(post)
-
-
-
+for entry in findRecord:
+    recordModel.append(f"{entry['model']}")
+    record.append(f"{entry['model']}")
+    record.append(f"{entry['miles']}")
+    record.append(f"{entry['date']}")
+    record.append(f"{entry['tasks']}")
+#df = pd.DataFrame([record])
