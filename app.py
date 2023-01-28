f64ba870f7c958dd1e4010a942bb8527b3a22ebd
Alec <alecshehorn97@gmail.com>
2023-01-28 14:09:27 -0600

    added DB to app.py, got all queries fixed

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-import pandas
+import pandas as pd
 import streamlit as st
 from db_create import *
+from pymongo import MongoClient
+from dotenv import load_dotenv, find_dotenv
+import os
+
 from bson.objectid import ObjectId
+#----DB setup---
+load_dotenv(find_dotenv())
+
+password = os.environ.get("MONGOPW")
+
+cluster = MongoClient(
+    f"""mongodb+srv://ashehorn:{password}@cluster0.4miwcyq.mongodb.net/?retryWrites=true&w=majority""")
+db = cluster["Maintenance"]
+vehicles = db["Vehicles"]
+records = db['Records']
+
+findVehicle = list(vehicles.find())
+
+selection = []
+recordModel = []
+record = []
+for item in findVehicle:
+    selection.append(f"{item['model']}")
+
 #---Page setup---
 title = st.title('Vehicle Maintenance Tracker')
 header = st.subheader('Here you can view, edit, and create Maintenance information related to your specific vehicle')
@@ -11,24 +34,33 @@ tab1, tab2, tab3 = st.tabs(["Add record", "View record", "Add Vehicle"])
 with tab1:
     with st.form('addrecord', clear_on_submit=True):
         vehicle = st.selectbox(
-            "Select a vehicle to add a record: ", (selection)
+            "Select a vehicle to add a record: ", selection
         )
-        miles = st.number_input("Enter miles of vehicle", key="miles", value=0, )
+        miles = str(st.number_input("Enter miles of vehicle", key="miles", value=0, ))
         date = str(st.date_input("What was the date of Maintenance? "))
         tasks = st.text_area("Tasks", placeholder="What tasks were performed? ")
+        if st.checkbox("Were parts used for this task?"):
+           parts = st.text_input("Enter Parts used")
+           if st.checkbox("Do you wish to enter the cost of parts?"):
+               cost = st.number_input()
         if st.form_submit_button("submit"):
             records.insert_one({"model": vehicle, "miles":miles, "date": date , "tasks":tasks})
             st.success("Record created")
 #---View Record---
 with tab2:
     with st.form("viewRecord"):
-        records = st.selectbox(
+        recordselect = st.selectbox(
             '',
             (selection)
         )
         "---"
         if st.form_submit_button("submit"):
-
+            x = []
+            findRecord = list(records.find({'model': recordselect}))
+            for entry in findRecord:
+                x.append(entry)
+            df = pd.DataFrame(x)
+            st.table(df)
 #---Add Vehicle---
 with tab3:
     with st.form("addVehicle", clear_on_submit=True):
@@ -42,6 +74,3 @@ with tab3:
             else:
                 vehicles.insert_one({"make": make, "model": model, "year": year})
                 st.success("Vehicle submitted")
-
-
-#if db.collection.count_documents({ 'UserIDS': newID }, limit = 1):
--- db_create.py
+++ db_create.py
@@ -1,29 +1,7 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv, find_dotenv
 from pymongo.collection import Collection
-import os
+
 import pandas as pd
 
-load_dotenv(find_dotenv())
 
-password = os.environ.get("MONGOPW")
-
-cluster = MongoClient(
-    f"""mongodb+srv://ashehorn:{password}@cluster0.4miwcyq.mongodb.net/?retryWrites=true&w=majority""")
-db = cluster["Maintenance"]
-vehicles = db["Vehicles"]
-records = db['Records']
-findVehicle = list(vehicles.find())
-findRecord = list(records.find())
-selection = []
-#db.Records.insert_one({"model":"tacoma", "miles": 230000,"date":"2023/24/1" ,"tasks":"oil change"})
-for item in findVehicle:
-    selection.append(f"{item['model']}")
-
-'''for entry in findRecord:
-    recordModel.append(f"{entry['model']}")
-    record.append(f"{entry['model']}")
-    record.append(f"{entry['miles']}")
-    record.append(f"{entry['date']}")
-    record.append(f"{entry['tasks']}")'''
-#df = pd.DataFrame([record])
