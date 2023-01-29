95a001a3113ec8232b36f366fad557fdefb1d85c
merderinmike <alecshehorn97@gmail.com>
2023-01-28 18:54:09 -0600

    Added refresh for adding vehicle, added remove vehicle function, also removes record when vehicle is removed

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import pandas as pd
+import time
 import streamlit as st
 from pymongo import MongoClient
 from dotenv import load_dotenv, find_dotenv
-#----DB setup---
+
+# ----DB setup---
 load_dotenv(find_dotenv())
 
 password = st.secrets["MONGOPW"]
-cluster = MongoClient(f"""mongodb+srv://ashehorn:{password}@cluster0.4miwcyq.mongodb.net/?retryWrites=true&w=majority""")
-
+cluster = MongoClient(
+    f"""mongodb+srv://ashehorn:{password}@cluster0.4miwcyq.mongodb.net/?retryWrites=true&w=majority""")
 
 db = cluster["Maintenance"]
 vehicles = db["Vehicles"]
@@ -19,12 +21,12 @@ record = []
 for item in findVehicle:
     selection.append(f"{item['model']}")
 
-#---Page setup---
+# ---Page setup---
 title = st.title('Vehicle Maintenance Tracker')
 header = st.subheader('Here you can view, edit, and create Maintenance information related to your specific vehicle')
-tab1, tab2, tab3 = st.tabs(["Add record", "View record", "Add Vehicle"])
+tab1, tab2, tab3, tab4 = st.tabs(["Add record", "View record", "Add Vehicle", "Remove Vehicle"])
 
-#---Add Record---
+# ---Add Record---
 with tab1:
     with st.form('addrecord', clear_on_submit=True):
         vehicle = st.selectbox(
@@ -33,19 +35,20 @@ with tab1:
         miles = str(st.number_input("Enter miles of vehicle", key="miles", value=0, ))
         date = str(st.date_input("What was the date of Maintenance? "))
         tasks = st.text_area("Tasks", placeholder="What tasks were performed? ")
-        if st.checkbox("Were parts used for this task?"):
-           parts = st.text_input("Enter Parts used")
-           if st.checkbox("Do you wish to enter the cost of parts?"):
-               cost = st.number_input()
+        check = st.checkbox("Were parts used for this task?")
+        if check:
+            st.text_input("Enter Parts used")
+            if st.checkbox("Do you wish to enter the cost of parts?"):
+                cost = st.number_input()
         if st.form_submit_button("submit"):
-            records.insert_one({"model": vehicle, "miles":miles, "date": date , "tasks":tasks})
+            records.insert_one({"model": vehicle, "miles": miles, "date": date, "tasks": tasks})
             st.success("Record created")
-#---View Record---
+# ---View Record---
 with tab2:
     with st.form("viewRecord"):
         recordselect = st.selectbox(
-            '',
-            (selection)
+            "Select a vehicle to view record",
+            (selection),
         )
         "---"
         if st.form_submit_button("submit"):
@@ -55,16 +58,29 @@ with tab2:
                 x.append(entry)
             df = pd.DataFrame(x)
             st.table(df)
-#---Add Vehicle---
+# ---Add Vehicle---
 with tab3:
     with st.form("addVehicle", clear_on_submit=True):
         make = st.text_input("Please enter the vehicle make: ", key="make")
         model = st.text_input("Please enter the vehicle model: ", key="model")
-        year = st.number_input("Please enter the vehicle year: ",key="year" ,value=0)
+        year = st.number_input("Please enter the vehicle year: ", key="year", value=0)
         submit = st.form_submit_button("submit")
         if submit:
-            if vehicles.count_documents({ 'model': model }) != 0:
+            if vehicles.count_documents({'model': model}) != 0:
                 st.exception("Vehicle already exists!")
             else:
                 vehicles.insert_one({"make": make, "model": model, "year": year})
                 st.success("Vehicle submitted")
+                time.sleep(1)
+                st.experimental_rerun()
+# ---Remove Vehicle---
+with tab4:
+    with st.form("removeVehicle", clear_on_submit=True):
+        vehicleRemove = st.selectbox(
+            "Select a vehicle to add a record: ", selection, label_visibility="visible"
+        )
+        if st.form_submit_button("Remove"):
+            vehicles.delete_many({"model": vehicleRemove})
+            st.success(f"{vehicleRemove} removed successfully")
+            time.sleep(1)
+            st.experimental_rerun()
