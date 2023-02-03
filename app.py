b2e9bf0167032390a3c75eadea77a1ae15e4a262
merderinmike <alecshehorn97@gmail.com>
2023-02-03 14:05:18 -0600

    Added error handling if action is selected and there is no record available for modification

--- app.py
+++ app.py
@@ -64,48 +64,51 @@ with tab2:
     df = pd.DataFrame(x)
     if recordselect != selection[0]:
         st.table(df)
-    action = st.selectbox("Edit or Remove a record?", ["Make Selection", "Edit", "Remove"])
+    action = st.selectbox("Edit or Remove a record?", ["Make Selection", "Edit", "Remove"], key="action")
     if action == "Edit":
         lst = []
-        findRecord = list(records.find({'model': recordselect}, {"_id": 0}))
-        for x in findRecord:
-            lst.append(x)
-        recordEdit = st.selectbox("Select your record", [lst[0]['tasks']])
-        checks = st.columns(4)
-        button1, button2 = st.columns(2)
-        with checks[0]:
-            checkTasks = st.checkbox("Tasks", key="Tasks")
-        with checks[1]:
-            checkMiles = st.checkbox("Miles", key="Miles")
-        with checks[2]:
-            checkParts = st.checkbox("Parts", key="Parts")
-        with checks[3]:
-            checkCost = st.checkbox("Cost", key="Cost")
-        with st.form("Edit"):
-            if checkTasks:
-                tasks = st.text_area("Tasks", placeholder="What tasks were performed? ", key="tasks1")
-            else:
-                tasks = lst[0]['tasks']
-            if checkMiles:
-                miles = str(st.number_input("Enter miles of vehicle", key="miles1", value=0, ))
-            else:
-                miles = lst[0]['miles']
-            if checkParts:
-                parts = st.text_area("Enter what parts were used for this task (If none leave blank)", key="parts1")
-            else:
-                parts = lst[0]['parts']
-            if checkCost:
-                cost = st.number_input("Enter total cost of task (Include any tools needed to complete task)",
-                                       key="cost1",
-                                       value=0,
-                                       min_value=0)
-            else:
-                cost = lst[0]['cost']
-            if st.form_submit_button("update"):
-                records.update_many({"$and": [{"tasks": recordEdit}]},
-                                    {"$set": {"miles": miles, "tasks": tasks, "parts": parts, "cost": cost}})
-            if st.form_submit_button("Submit", on_click=clearCheck):
-                st.experimental_rerun()
+        try:
+            findRecord = list(records.find({'model': recordselect}, {"_id": 0}))
+            for x in findRecord:
+                lst.append(x)
+            recordEdit = st.selectbox("Select your record", [lst[0]['tasks']])
+            checks = st.columns(4)
+            button1, button2 = st.columns(2)
+            with checks[0]:
+                checkTasks = st.checkbox("Tasks", key="Tasks")
+            with checks[1]:
+                checkMiles = st.checkbox("Miles", key="Miles")
+            with checks[2]:
+                checkParts = st.checkbox("Parts", key="Parts")
+            with checks[3]:
+                checkCost = st.checkbox("Cost", key="Cost")
+            with st.form("Edit"):
+                if checkTasks:
+                    tasks = st.text_area("Tasks", placeholder="What tasks were performed? ", key="tasks1")
+                else:
+                    tasks = lst[0]['tasks']
+                if checkMiles:
+                    miles = str(st.number_input("Enter miles of vehicle", key="miles1", value=0, ))
+                else:
+                    miles = lst[0]['miles']
+                if checkParts:
+                    parts = st.text_area("Enter what parts were used for this task (If none leave blank)", key="parts1")
+                else:
+                    parts = lst[0]['parts']
+                if checkCost:
+                    cost = st.number_input("Enter total cost of task (Include any tools needed to complete task)",
+                                           key="cost1",
+                                           value=0,
+                                           min_value=0)
+                else:
+                    cost = lst[0]['cost']
+                if st.form_submit_button("update"):
+                    records.update_many({"$and": [{"tasks": recordEdit}]},
+                                        {"$set": {"miles": miles, "tasks": tasks, "parts": parts, "cost": cost}})
+                if st.form_submit_button("Submit", on_click=clearCheck):
+                    st.experimental_rerun()
+        except:
+            st.error("There are no records to view or modify")
 
     elif action == "Remove":
         lst = []
@@ -126,7 +129,6 @@ with tab2:
 
         except:
             st.error("There are no records for this vehicle to be removed")
-
 # ---Add Vehicle---
 with tab3:
     with st.form("addVehicle", clear_on_submit=True):
